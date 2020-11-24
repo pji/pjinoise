e9adb97d36ec5a8bb63e8715eb26cb0b670fb986
Paul J. Iutzi <pji@mac.com>
2020-11-24 00:18:33 -0600

    Added save_conf to io.

--- pjinoise/io.py
+++ pjinoise/io.py
@@ -8,6 +8,7 @@ import json
 from typing import Mapping
 
 from pjinoise import model as m
+from pjinoise.__version__ import __version__
 
 
 def load_conf(filename: str) -> m.Image:
@@ -21,5 +22,25 @@ def load_conf(filename: str) -> m.Image:
     if conf['Version'] == '0.2.0':
         return m.Image(**conf['Image'])
     
+    # Otherwise, the version isn't recognized, so throw an error.
     else:
         raise ValueError(f'Version {conf["Version"]} not supported.')
+
+
+def save_conf(conf: m.Image) -> None:
+    """Save the configuration file."""
+    # Determine the name of the config file.
+    parts = conf.filename.split('.')[:-1]
+    parts.append('json')
+    conffile = '.'.join(parts)
+    
+    # Serialize the config.
+    confmap = {
+        'Version': __version__,
+        'Image': conf.asdict(),
+    }
+    confjson = json.dumps(confmap, indent=4)
+    
+    # Save the config.
+    with open(conffile, 'w') as fh:
+        fh.write(confjson)
--- tests/test_io.py
+++ tests/test_io.py
@@ -12,6 +12,7 @@ from pjinoise import io
 from pjinoise import model as m
 from pjinoise import operations as op
 from pjinoise import sources as s
+from pjinoise.__version__ import __version__
 
 
 class IOTestCase(ut.TestCase):
@@ -63,4 +64,56 @@ class IOTestCase(ut.TestCase):
         
         # Determine if test passed.
         self.assertEqual(exp_conf, act_conf)
+        open_mock.assert_called_with(*exp_args)
+    
+    def test_serialize_config_to_json_file(self):
+        """Given a configuration object, serialize that object to 
+        file as JSON.
+        """
+        # Set up for expected data.
+        format = 'JPEG'
+        filename = 'spam.json'
+        framerate = None
+        imagefile = 'spam.jpeg'
+        mode = 'RGB'
+        size = [1, 1280, 720]
+        conf = m.Image(**{
+            'source': m.Layer(**{
+                'source': s.Spot(**{
+                    'radius': 128,
+                    'ease': 'l',
+                }),
+                'filters': [],
+                'mask': None,
+                'mask_filters': [],
+                'blend': op.difference,
+                'blend_amount': 1.0,
+            }),
+            'size': size,
+            'filename': imagefile,
+            'format': format,
+            'mode': mode,
+            'framerate': None
+        })
+        serialized_conf = {
+            'Version': __version__,
+            'Image': conf.asdict()
+        }
+        
+        # Expected values.
+        exp_json = json.dumps(serialized_conf, indent=4)
+        exp_args = (filename, 'w')
+        
+        # Set up test data and state.
+        open_mock = mock_open()
+        with patch('pjinoise.io.open', open_mock, create=True):
+        
+            # Run test.
+            io.save_conf(conf)
+        
+        # Extract actual values.
+        act_json = open_mock.return_value.write.call_args[0][0] 
+        
+        # Determine if test passed.
+        self.assertEqual(exp_json, act_json)
         open_mock.assert_called_with(*exp_args)
